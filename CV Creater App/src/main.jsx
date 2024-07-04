6be0b0dd3e169b953867bf74ac1d77fb95343baf
Gourav Sharma <gourav.sharma.glamat2932@gmail.com>
2024-07-04 13:15:02 +0530

    Fixed the background and props

--- CV Creater App/src/main.jsx
+++ CV Creater App/src/main.jsx
@@ -8,6 +8,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <InfoPanel width={40}/>
+    <InfoPanel width={40} borderRadius="30px 0 0 30px" color="#16161D"/>
+    <InfoPanel width={60} borderRadius="0 30px 30px 0"/>
   </React.StrictMode>,
 )
