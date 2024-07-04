28fbcd0014a7de378ca9627b2d756c815948df90
Gourav Sharma <gourav.sharma.glamat2932@gmail.com>
2024-07-04 16:08:21 +0530

    added map

--- CV Creater App/src/component's/Personl.jsx
+++ CV Creater App/src/component's/Personl.jsx
@@ -1,17 +1,13 @@
 import '../style/Personal.css'
 
-function Personal(props){
+function Personal({text, input}){
     return (
         <div className="personal">
-            <h1>{props.text}</h1>
-            <hr/><br/>
-            <label>Full Name</label>
-            <input placeholder={props.text} /><br/>
-            <label>Email</label>
-            <input  /><br/>
-            <label>Phone</label>
-            <input  /><br></br>
-            <button>Save</button>
+            <h1>{text}</h1>
+            <hr/>
+            {input.map((field) => {
+                return <input placeholder={field} />
+            })}
         </div>
     )
 }
