a4c707fe0cd43b11ae97368838c58ba2297df465
Gourav Sharma <gourav.sharma.glamat2932@gmail.com>
2024-07-04 23:27:34 +0530

    Update main.jsx

--- CV Creater App/src/main.jsx
+++ CV Creater App/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import InfoPanel from './component\'s/Info.jsx'
-import Personal from './component\'s/Personl.jsx'
+import {InfoPanel, ShowPanel} from './component\'s/Info.jsx'
+// import ShowPanel from './component\'s/Show.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <InfoPanel width={40} borderRadius="30px 0 0 30px" color="#16161D"/>
+    <ShowPanel width={60} borderRadius="0px 30px 30px 0px" color="gray"/>
   </React.StrictMode>,
 )
