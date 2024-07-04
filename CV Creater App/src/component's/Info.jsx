0d3678037d69d76389061672f66da864c078c570
Gourav Sharma <gourav.sharma.glamat2932@gmail.com>
2024-07-04 23:29:01 +0530

    Update Info.jsx

--- CV Creater App/src/component's/Info.jsx
+++ CV Creater App/src/component's/Info.jsx
@@ -1,4 +1,5 @@
 import '../style/Info.css';
+import { useState } from 'react';
 import Personal from './Personl';
 import Button from './Button'
 
@@ -10,19 +11,42 @@ function InfoPanel(props){
     }
 
     let personalList = ['FullName', 'Email', 'Phone'];
-    let educationList = ['Course','School','Year Started','Year Ended']
-    let workList = ['Company Name', 'Position', 'Job Description', 'Year Started', 'Year Ended'];
+    let educationList = ['Course','School','Edu. Year Started','Edu. Year Ended']
+    let workList = ['Company Name', 'Position', 'Job Description', 'Job Year Started', 'Job Year Ended'];
 
     return (
-        <div className="infoPanel flex" style={background}>
+        <form className="infoPanel flex" style={background} >
             <Personal text="Personal" input={personalList} />
             <Personal text="Education" input={educationList} />
             <Personal text="Work" input={workList} />
             <div className="button flex">
-                <Button text="Save" /><Button text="Edit" />
+                <Button type="submit" text="Save" /><Button text="Edit" />
+            </div>
+        </form>
+    )
+}
+function ShowPanel(props){
+
+    let background = {
+        width: props.width + '%',
+        borderRadius: props.borderRadius,
+        backgroundColor: props.color
+    }
+
+    return (
+        <div className="infoPanel flex" style={background}>
+            <div className="cv">
+                <div className="header">
+                    <h1>Name</h1>
+                    <div className="contact">
+                        <h5>Contact</h5>
+                        <p>Phone</p>
+                        <p>Email</p>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
 }
-
-export default InfoPanel;
+export {InfoPanel, ShowPanel};
